e0da1613e60a66bcd240a94cc9c1e61769212d7b
searchme86 <searchme86@gmail.com>
2023-05-03 17:10:59 +0900

    ➕ tailwindCSS npm 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TestIcon from './pages/TestIcon';
+import TestTailwindcss from './pages/TestTailwindcss';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
       <TestIcon />
+      <TestTailwindcss />
     </div>
   );
 }
