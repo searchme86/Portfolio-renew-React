37826b6ce5cd8b356549df5ca8486f5e26eaac81
searchme86 <searchme86@gmail.com>
2023-05-04 11:47:17 +0900

    ✨ tailwindcss 커스텀 폰트사이즈 사용가능하도록 설정변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontSize: {
+      '16px': '1rem',
+      '18px': '1.125rem',
+      '20px': '1.25rem',
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/line-clamp'), require('daisyui')],
